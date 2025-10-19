d76927a7a157a959118d772bc0991ad31f921eab
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-10-19 20:01:45 +0900

    feat(validation): add escapeForCharClass method to handle regex metacharacters

--- src/main/java/calculator/AddRequest.java
+++ src/main/java/calculator/AddRequest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class AddRequest {
 
@@ -34,14 +35,32 @@ public class AddRequest {
         return numbers;
     }
 
+
+    /**
+     * 정규표현식 안에 \, ], -, ^이 있을 때 인식을 혼동하는 경우를 방지하기위해 \을 앞에 붙이는 기능
+     *
+     * @param str 입력받은 문자열
+     * @return 수정한 문자열
+     */
+    private static String escapeForCharClass(String str) {
+        return str
+                .replace("\\", "\\\\")  // 백슬래시
+                .replace("]", "\\]")    // 닫는 대괄호
+                .replace("[", "\\[")    // 닫는 대괄호
+                .replace("-", "\\-")    // 하이픈
+                .replace("^", "\\^");   // 캐럿 (맨 앞에 오는 경우 대비)
+    }
+
+
     /**
      * 문자열을 입력 받고, 구분자로 구분하여 숫자만 numbers에 저장한다.
      */
     public void inputNumbers() throws IllegalArgumentException {
-        customValidation(input);
+        customValidation();
         String inputNumbers = input.substring(divisionIndex.inputStartIndex, divisionIndex.inputEndIndex);
         String inputSeparators =
-                ":," + input.substring(divisionIndex.separatorStartIndex, divisionIndex.separatorEndIndex);
+                ":," + escapeForCharClass(
+                        input.substring(divisionIndex.separatorStartIndex, divisionIndex.separatorEndIndex));
 
         validate(inputNumbers, inputSeparators);
 
@@ -53,17 +72,15 @@ public class AddRequest {
     /**
      * 커스텀 구분자가 양쪽 다 있으면 시작, 끝 인덱스 반환 아니면 예외 던지기
      *
-     * @param input 입력값
-     * @return (separator 시작 인덱스, separator 끝나는 인덱스, input 시작 인덱스, input 끝 인덱스)
      * @throws IllegalArgumentException 입력 잘못 들어온 예외
      */
-    private void customValidation(String input) throws IllegalArgumentException {
+    private void customValidation() throws IllegalArgumentException {
         if (input == null) {
-            throw new IllegalArgumentException("Input is null");
+            input = "0";
         }
 
         if (input.isEmpty()) {
-            throw new IllegalArgumentException("Input is empty");
+            input = "0";
         }
 
         int startIndex = input.indexOf(START_SEPARATOR);
@@ -96,7 +113,7 @@ public class AddRequest {
      */
     private void validate(String numbers, String separators) throws IllegalArgumentException {
         // numbers 숫자, 구분자(separators) 이외의 값이 있으면
-        if (!(numbers.matches("^[0-9" + separators + "]*$"))) {
+        if (!(numbers.matches("^[0-9" + Pattern.quote(separators) + "]*$"))) {
             throw new IllegalArgumentException("Invalid input");
         }
         // 구분자에 숫자 있으면
